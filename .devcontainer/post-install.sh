a21b7fd70835f11c0d77465414ce2f659b53f4e2
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2023-02-01 13:57:04 -0800

    Update post-install.sh

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -21,4 +21,4 @@ VERSION=$(getLatestVersion)
 cd ~
 wget "https://releases.hashicorp.com/terraform/"$VERSION"/terraform_"$VERSION"_linux_amd64.zip"
 unzip "terraform_"$VERSION"_linux_amd64.zip"
-sudo mv terraform /usr/local/bin/terraform
+sudo install terraform /usr/local/bin/
